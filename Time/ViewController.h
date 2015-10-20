43754392564d4e69de696cb5f4696920be91d33e
sipo <sipo@bnu.edu.cn>
2015-10-20 15:46:43 +0800

    Initial Commit

---
+++ Time/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Time
+//
+//  Created by sipo on 15/10/20.
+//  Copyright (c) 2015年 sipo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Time/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Time
+//
+//  Created by sipo on 15/10/20.
+//  Copyright (c) 2015年 sipo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
